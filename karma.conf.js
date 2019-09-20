041b4b8ba75c94c6adff20fff7635888c5a6e9e7
blavbla <bla@bla.com>
2019-09-20 11:27:24 +0700

    blabla

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+module.exports = function (config) {
+    config.set({
+        frameworks: [
+            'mocha',
+            'chai',
+            'detectBrowsers'
+        ],
+
+        files: [
+            'test/sqrt.js'
+        ],
+
+        client: {
+            mocha: {
+                reporter: 'html',
+                ui: 'bdd'
+            }
+        },
+
+        singleRun: true,
+        colors: true,
+
+        detectBrowsers: {
+            enabled: true,
+            usePhantomJS: false
+        },
+
+        plugins: [
+            'karma-mocha',
+            'karma-chai',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-ie-launcher',
+            'karma-safari-launcher',
+            'karma-detect-browsers'
+        ]
+    });
+}
